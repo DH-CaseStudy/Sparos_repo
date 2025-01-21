a6c7c69c137a45542bf3c179c481ecef39e4e27f
cksael@gmail.com <cksael@gmail.com>
2025-01-21 11:41:22 +0900

    day10 - 오버로딩을 통한 다형성 학습

--- Study/src/day10/Person.java
+++ Study/src/day10/Person.java
@@ -4,10 +4,26 @@ public class Person {
     public String name;
     public int age;
     public String phoneNumber;
+    public String gender;
 
     //디폴트 생성자.
     public Person(){
 
+    }
+
+    //디폴트 생성자.
+    public Person(String _name, int _age, String _phoneNumber){
+        this.name = _name;
+        this.age = _age;
+        this.phoneNumber = _phoneNumber;
+    }
+
+    //오버로딩
+    public Person(String _name, int _age, String _phoneNumber, String _gender){
+        this.name = _name;
+        this.age = _age;
+        this.phoneNumber = _phoneNumber;
+        this.gender = _gender;
     }
 
     public void work(){
---
+++ Study/src/day10/PersonMain.java
@@ -0,0 +1,8 @@
+package day10;
+
+public class PersonMain {
+    public static void main(String[] args) {
+        Person person1 = new Person("이동휘", 33, "010");
+        Person person2 = new Person("이동휘", 33, "010", "male");
+    }
+}
