a9f87f92a8675b292bc1eb616f9b685d54cdeeb5
cksael@gmail.com <cksael@gmail.com>
2025-02-04 09:58:06 +0900

    shape 과제 수정

--- Java_advanced/src/day01/homework/shapeEX/TestShape.java
+++ Java_advanced/src/day01/homework/shapeEX/TestShape.java
@@ -4,11 +4,15 @@ public class TestShape {
 	public static void main(String[] args) {
 		Shape[] shape = new Shape[2];
 
-		shape[0] = new Circle(100);
+		shape[0] = new Circle(10);
 		shape[0].calculationArea();
+		System.out.printf("원의 면적은 ");
+		shape[0].print();
 
 		shape[1] = new Rectangular(100.0, 2.0);
 		shape[1].calculationArea();
+		System.out.printf("직사각형의 면적은 ");
+		shape[1].print();
 		
 	}
 }
